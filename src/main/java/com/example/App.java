a0eba89dd4c667d3c0e48741e6e9b614392c7b3a
Ubuntu <ubuntu@ip-172-31-3-208.ap-south-1.compute.internal>
2025-11-04 17:14:25 +0000

    webhook project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI/CD with Webhook!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
+
